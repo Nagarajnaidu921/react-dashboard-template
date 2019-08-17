901fa248c1243f2f635eba22b6d4140a1aa6dd01
nagaraj <nagarajnaidu921@gmail.com>
2019-08-17 20:05:04 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import './App.css';
 import Layout from './components/Layout';
+import SignIn from './pages/Login';
+import SignupForm from './components/SignupForm'
+
+const routes = [
+
+  {
+    path: '/login',
+    component: SignIn,
+  },
+  {
+    path: '/signup',
+    component: SignupForm,
+  },
+
+]
 
 function App() {
   return (
     <div className="App">
       <Layout>
-            
+            <Router>
+            <Route path="/" exact component={SignIn} />
+              {routes.map(x => (
+                <Route {...x} />
+              ))}
+            </Router>
       </Layout>
     </div>
   );
