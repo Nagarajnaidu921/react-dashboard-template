58865f82d2739445c9153f232bef37098c09ea80
vemulanagaraj <nagaraj.v@hcl.com>
2019-08-18 18:31:28 +0530

    added secure route

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 
 import './App.css';
 import Layout from './components/Layout';
 import SignIn from './pages/Login';
 import Home from './pages/Home';
+import Users from './pages/Users';
 import SignupForm from './components/SignupForm'
+import AuthServ from './services/Auth/auth.service';
 
 const routes = [
 
   {
     path: '/login',
     component: SignIn,
+    isSecure: false
   },
   {
     path: '/signup',
     component: SignupForm,
+    isSecure: false
+  },
+  {
+    path: '/users',
+    component: Users,
+    isSecure: true
   },
 
 ]
+
+const SecureRoute = ({component: Component, ...rest}) => (
+  <Route {...rest} render={(props) => (
+    AuthServ.isLoggedIn ?  <Component {...props} /> :  <Redirect to='/login' />
+)}/>
+)
 
 function App() {
   return (
     <div className="App">
       <Layout>
-
             <Route path="/" exact component={Home} />
-              {routes.map(x => (
-                <Route {...x} />
+              {routes.map((x, id) => (
+                x.isSecure === true ? <SecureRoute key={id} {...x}/> :
+                <Route key={id} {...x} />
               ))}
 
       </Layout>
