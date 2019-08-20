c6ee3895cbb5611cb2a3f2dadfbce6ce9082c001
vemulanagaraj <nagaraj.v@hcl.com>
2019-08-20 13:12:54 +0530

    code readability patch

--- src/services/Auth/auth.service.js
+++ src/services/Auth/auth.service.js
@@ -2,9 +2,15 @@ import axios from 'axios';
 
 import  SettingsServ from '../Settings/settings.service';
 import TokenServ from '../Token/token.service';
+import { CallToActionSharp } from '@material-ui/icons';
 
 class AuthServ {
     HOST = SettingsServ.HOST;
+
+    API_URL = {
+        signup: `${this.HOST}/api/auth/signup`,
+        login: `${this.HOST}/api/auth/login`
+    }
 
     get isLoggedIn() {
         return TokenServ.token;
@@ -14,9 +20,15 @@ class AuthServ {
         TokenServ.removerToken();
     }
 
-    API_URL = {
-        signup: `${this.HOST}/api/auth/signup`,
-        login: `${this.HOST}/api/auth/login`
+    getUserInfo() {
+        const token = TokenServ.token || '';
+        const [, tokenData, ] = token.split('.');
+        try {
+            const { id, firstName, lastName, email } =  JSON.parse(window.atob(tokenData));
+            return { id, firstName, lastName, email }; 
+        } catch(error) {
+            console.log(error)
+        }
     }
 
     async signUp(data) {
